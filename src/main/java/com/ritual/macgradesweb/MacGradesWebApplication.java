fadf1bd532da8018b8847d81007a044f3dc9ec36
ethan <pattee14@mcmaster.ca>
2023-06-08 14:54:49 -0400

    Initial commit

---
+++ src/main/java/com/ritual/macgradesweb/MacGradesWebApplication.java
@@ -0,0 +1,13 @@
+package com.ritual.macgradesweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacGradesWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MacGradesWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ritual/macgradesweb/MacGradesWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ritual.macgradesweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacGradesWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
