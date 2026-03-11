06f5a3c9e1697f07511ee9a1d3abb867861056cc
Ethan Patterson <pattee14@mcmaster.ca>
2026-03-11 16:16:15 -0400

    Remove unnecessary effect

--- src/Components/Home/GpaDisplay.js
+++ src/Components/Home/GpaDisplay.js
@@ -1,8 +1,48 @@
-import React, { useState, useEffect, useContext } from "react";
+import { useContext } from "react";
 
 import { CourseListContext } from "../../Contexts/CourseListContext";
 import "./GpaDisplay.css";
 import Card from "./Card";
+
+export default function GpaDisplay() {
+  const [courses] = useContext(CourseListContext);
+  const standardGPA = calculateGPA(courses, standardGrades);
+  const macGPA = calculateGPA(courses, macGrades);
+
+  return (
+    <>
+      <Card>
+        <div className="gpa-title">4.0 Scale GPA</div>
+
+        <div className="gpa gpa-num">{standardGPA.toFixed(2)}</div>
+      </Card>
+      <Card>
+        <div className="gpa-title">McMaster GPA</div>
+
+        <div className="gpa gpa-num">{macGPA.toFixed(1)}</div>
+      </Card>
+    </>
+  );
+}
+
+function calculateGPA(courses, gradeConversion) {
+  if (courses.length === 0) return 0.0;
+
+  let pointsAttm = 0.0;
+  let pointsEarned = 0.0;
+
+  courses.forEach((course) => {
+    let grade = parseFloat(gradeConversion[course.grade]);
+    let units = parseFloat(course.units);
+    pointsEarned += units * grade;
+    pointsAttm += units;
+  });
+
+  if (pointsAttm === 0) {
+    return 0.0;
+  }
+  return pointsEarned / pointsAttm;
+}
 
 const macGrades = {
   "A+": 12,
@@ -35,59 +75,3 @@ const standardGrades = {
   "D-": 0.7,
   F: 0,
 };
-
-export default function GpaDisplay() {
-  const [courses] = useContext(CourseListContext);
-  const [standardGPA, setStandardGPA] = useState(0.0);
-  const [macGPA, setMacGPA] = useState(0.0);
-
-  function calculateGPA(gradeConversion) {
-    let pointsAttm = 0.0;
-    let pointsEarned = 0.0;
-
-    courses.forEach((course) => {
-      let grade = parseFloat(gradeConversion[course.grade]);
-      let units = parseFloat(course.units);
-      pointsEarned += units * grade;
-      pointsAttm += units;
-    });
-    return pointsEarned / pointsAttm;
-  }
-
-  useEffect(() => {
-    setStandardGPA(calculateGPA(standardGrades));
-    setMacGPA(calculateGPA(macGrades));
-    if (courses.length === 0) {
-      setStandardGPA(0.0);
-      setMacGPA(0.0);
-    }
-  }, [courses]);
-
-  return (
-    <>
-      <Card>
-        <div className="gpa-title">4.0 Scale GPA</div>
-
-        <div className="gpa gpa-num">{standardGPA.toFixed(2)}</div>
-      </Card>
-      <Card>
-        <div className="gpa-title">McMaster GPA</div>
-
-        <div className="gpa gpa-num">{macGPA.toFixed(1)}</div>
-      </Card>
-      {/* <InlineCard> */}
-      {/*   <InlineCardContent> */}
-      {/*     <div className="gpa">4.0 Scale GPA</div> */}
-      {/**/}
-      {/*     <div className="gpa gpa-num">{standardGPA.toFixed(2)}</div> */}
-      {/*   </InlineCardContent> */}
-      {/*   <InlineCardContent> */}
-      {/*     <div className="gpa">McMaster GPA</div> */}
-      {/**/}
-      {/*     <div className="gpa gpa-num">{macGPA.toFixed(1)}</div> */}
-      {/*   </InlineCardContent> */}
-      {/*   <span /> */}
-      {/* </InlineCard> */}
-    </>
-  );
-}
