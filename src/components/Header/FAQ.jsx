10c855101c479a8d7c7c1fd63b99c66f40667b66
Ethan Patterson <ethanpatterson88@gmail.com>
2026-05-28 19:17:44 -0400

    Add faq icon

---
+++ src/components/Header/FAQ.jsx
@@ -0,0 +1,12 @@
+import { ReactComponent as QuestionMarkIcon } from "assets/question-mark-circled-svgrepo-com.svg";
+import "./FAQ.css";
+
+const FAQ = () => {
+  return (
+    <>
+      <QuestionMarkIcon width={30} height={30} className="faq-icon" />
+    </>
+  );
+};
+
+export default FAQ;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,16 @@
+import FAQ from "components/Header/FAQ";
 import "./Header.css";
 
 const Header = () => {
   return (
     <header className="banner">
-      <h1>MacGrades</h1>
-      <p>McMaster GPA Calculator</p>
+      <div className="title">
+        <h1>MacGrades</h1>
+        <p>McMaster GPA Calculator</p>
+      </div>
+      <FAQ />
     </header>
   );
 };
 
 export default Header;
-
